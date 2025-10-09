380b7102ffecb230470156cc626d06adf94a543a
YammD123 <siamqueen52@gmail.com>
2025-10-09 22:23:11 +0700

    chore: remove unused dependencies and controllers related to Samehadaku

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { Hono } from "hono";
-import { SamehadakuRoute } from "./routes/SamehadakuRoute";
 import { upgradeWebSocket, websocket } from "hono/bun";
 
 const app = new Hono().basePath("/api");
 
 // route lain (misal samehadaku)
-app.route("anime/samehadaku", SamehadakuRoute);
 
 // penting: export server config
 export default {
--- src/routes/SamehadakuRoute.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { SameHadakuController } from "../controllers/SamehadakuController";
-import { upgradeWebSocket, websocket } from 'hono/bun'
-
-const samehadakuController = new SameHadakuController()
-export const SamehadakuRoute = new Hono()
