bd8fa66083b8d8bd5a5707a2e69cf0f242580c42
YammD123 <siamqueen52@gmail.com>
2025-10-24 19:27:05 +0700

    feat: add CORS middleware to enable cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { authController } from "./api/controllers/AuthController";
 import { HTTPException } from 'hono/http-exception'
 import { contactController } from "./api/controllers/ContactController";
 import { chatController } from "./api/controllers/ChatController";
-
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api");
 
@@ -18,13 +18,19 @@ app.onError((err, c) => {
 
   return c.text('Internal Server Error', 500)
 })
+app.use("*",cors({
+  origin: ["http://localhost:5173","http://localhost:4173"],
+  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}))
+
+
 //kumpulan route
 app.get("/ping", (c) => c.text("pong"));
 app.route("/ws", wsController);
 app.route("/auth", authController)
 app.route("/contacts", contactController);
 app.route("/chats", chatController);
-
 // penting: export server config
 export default {
   port: 4000,
