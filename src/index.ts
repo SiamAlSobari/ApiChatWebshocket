d03c20b25b7252a5b7d2d0ae341e0227b45178ae
YammD123 <siamqueen52@gmail.com>
2025-10-26 21:03:32 +0700

    feat: update dependencies, implement JWT authentication, and add user/profile models

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import { Elysia, t } from "elysia";
-import { ApiResponseModel } from "./utils/response/model";
-import { ApiResponse } from "./utils/response/api";
-import { authMiddleware } from "./utils/middlewares/auth.middleware";
 import { Auth } from "./modules/auth";
 
-const app = new Elysia({ prefix: "/api" })
+const clients: any[] = []; // Simpan semua koneksi aktif
+const app = new Elysia()
   .get("/", () => "Hello Elysia")
+  //   .ws("/chat", {
+  //   open(ws) {
+  //     clients.push(ws);
+  //     console.log("✅ Client connected, total:", clients.length);
+  //     ws.send("Welcome to Elysia Chat 💬");
+  //   },
+
+  //   message(ws, msg) {
+  //     console.log("📩 Message:", msg);
+
+  //     // Kirim pesan ke semua client (termasuk pengirim)
+  //     for (const client of clients) {
+  //       if (client.readyState === 1) {
+  //         client.send(String(msg));
+  //       }
+  //     }
+  //   },
+
+  //   close(ws) {
+  //     // Hapus koneksi dari daftar
+  //     const index = clients.indexOf(ws);
+  //     if (index !== -1) clients.splice(index, 1);
+  //     console.log("❌ Client disconnected, total:", clients.length);
+  //   },
+  // })
+  .group("/api", (app) =>
+    app.use(Auth)
+  )
   .use(Auth)
   .listen(3000);
 
--- src/utils/middlewares/auth.middleware.ts
+++ src/utils/middlewares/auth.middleware.ts
@@ -1,15 +1,28 @@
 // auth.middleware.ts
 import { Elysia } from "elysia";
 import { Exception } from "../response/exception";
+import jwt from "@elysiajs/jwt";
+import { SECRET_KEY } from "../constant/secret";
 
-export const authMiddleware = (app: Elysia) =>
-    app.derive(({cookie}) => {
-        // Middleware logic here
+export const authMiddleware =  async (app: Elysia) =>
+    app.use(jwt({
+        secret: SECRET_KEY,
+        name: "jwt"
+    }))
+    .derive(({ cookie, jwt }) => {
         const token = cookie?.token?.value;
         if (!token) {
             throw new Exception("Unauthorized", 401);
         }
-        return {
-            user: "AuthenticatedUser"
+        try {
+            const payload = jwt.verify(token as string);
+            if (!payload) {
+                throw new Exception("Unauthorized", 401);
+            }
+            return {
+                user: payload
+            }
+        } catch (error) {
+            throw new Exception("Unauthorized", 401);
         }
     })
