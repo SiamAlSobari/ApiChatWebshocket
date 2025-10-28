f013132f91132ccf92512ad4d57b9f7eff83251e
YammD123 <siamqueen52@gmail.com>
2025-10-28 22:16:36 +0700

    fix: memperbaiki getcontact diamana dia ga mengembalkan id dari table user

--- src/modules/contact/repository.ts
+++ src/modules/contact/repository.ts
@@ -1,21 +1,29 @@
 import { db } from "../../utils/constant/db";
 
 export class ContactRepository {
-    public async createContact(contactName: string, contactId:string, userId: string) {
+    public async createContact(contactName: string, contactId: string, userId: string) {
         return await db.contact.create({
             data: {
                 contact_name: contactName,
                 contact_id: contactId,
-                user_id: userId
-            }
-        })
+                user_id: userId,
+            },
+        });
     }
 
     public async getContact(userId: string) {
         return await db.contact.findMany({
             where: {
-                user_id: userId
-            }
-        })
+                user_id: userId,
+            },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        profile: true,
+                    },
+                },
+            },
+        });
     }
 }
